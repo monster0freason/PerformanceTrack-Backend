31ea01deb3deb80e595c1bc519a3387a1c625f90
Rudra-cogz <Rudrapratap.SinghBohra@cognizant.com>
2026-01-28 15:13:31 +0530

    Inserted data in two goals entity & five enums

---
+++ src/main/java/com/project/performanceTrack/enums/EvidenceVerificationStatus.java
@@ -0,0 +1,8 @@
+package com.project.performanceTrack.enums;
+
+public enum EvidenceVerificationStatus {
+    NOT_VERIFIED,
+    VERIFIED,
+    NEEDS_ADDITIONAL_LINK,
+    REJECTED
+}
---
+++ src/main/java/com/project/performanceTrack/enums/GoalCategory.java
@@ -0,0 +1,8 @@
+package com.project.performanceTrack.enums;
+
+public enum GoalCategory {
+    TECHNICAL,
+    BEHAVIORAL,
+    PROFESSIONAL_DEVELOPMENT,
+    OTHER
+}
